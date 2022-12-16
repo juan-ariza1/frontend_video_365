b1e345b9f145e38056996b816e17910964d9e1aa
edwincj2 <edwincj2@hotmail.com>
2022-12-15 20:46:22 -0500

    [feature] se creo el componente movie y topMenu para el proyecto de Video365

---
+++ src/components/menu/TopMenu.js
@@ -0,0 +1,127 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./TopMenu.css";
+import { API_URL, isAuth } from "../../util/Util";
+
+export const TopMenu = () => {
+  const [categories, setCategories] = useState([]);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    getCategoriesAsync();
+  }, []);
+
+  const getCategoriesAsync = async () => {
+    let response = await fetch(API_URL + "category");
+    response = await response.json();
+    setCategories(response);
+  };
+
+  const items = [
+    "Inicio",
+    "Categorías",
+    "Más vistas",
+    "Mis listas",
+    "Mis calificados",
+    "Mi cuenta",
+  ];
+
+  const clasicMenu = () => (
+    <div className="navbar">
+      <Link to="/">Inicio</Link>
+      <div className="subnav">
+        <button className="subnavbtn">
+          Categorías <i className="fa fa-caret-down"></i>
+        </button>
+        <div className="subnav-content">
+          {categories.map((item, idx) => (
+            <Link key={idx} to={`/category/${item.name}`}>
+              {item.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="subnav">
+        <button className="subnavbtn">
+          Partners <i className="fa fa-caret-down"></i>
+        </button>
+        <div className="subnav-content">
+          <a href="#link1">Link 1</a>
+          <a href="#link2">Link 2</a>
+          <a href="#link3">Link 3</a>
+          <a href="#link4">Link 4</a>
+        </div>
+      </div>
+      <a href="#contact">Contact</a>
+    </div>
+  );
+
+  const oldMenu = () => (
+    <div classNameName="scrollmenu">
+      {items.map((item, idx) => (
+        <a key={idx} href="#home">
+          {item}
+        </a>
+      ))}
+    </div>
+  );
+
+  const logOut = () => {
+    localStorage.clear();
+    navigate("/");
+  };
+
+  const renderLogout = () => {
+    if (localStorage.getItem("authData")) {
+      return <a onClick={logOut}>Cerrar sesión</a>;
+    }
+  };
+
+  const bootstrapMenu = () => (
+    <Navbar bg="light" expand="lg">
+      {isAuth() ? (
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">
+                <Link to="/">Inicio</Link>
+              </Nav.Link>
+              <NavDropdown title="Categorías" id="basic-nav-dropdown">
+                {categories.map((item, idx) => (
+                  <NavDropdown.Item key={idx}>
+                    <Link key={idx} to={`/category/${item.name}`}>
+                      {item.name}
+                    </Link>
+                  </NavDropdown.Item>
+                ))}
+              </NavDropdown>
+              <Nav.Link>
+                <Link to={`/category/`}>Más vistas</Link>
+              </Nav.Link>
+              <Nav.Link href="#link">
+                <Link to={`/view`}>Mis listas</Link>
+              </Nav.Link>
+              <Nav.Link href="#link">
+                <Link to={`/scores`}>Mis calificados</Link>
+              </Nav.Link>
+              <Nav.Link href="#link">
+                <Link to={`/account`}>Mi cuenta</Link>
+              </Nav.Link>
+              <Nav.Link href="#link">{renderLogout()}</Nav.Link>
+            </Nav>
+         </Navbar.Collapse>
+       </Container>
+      ) : (
+        ""
+      )}
+    </Navbar>
+  );
+
+  return bootstrapMenu();
+};
---
+++ src/components/movie/movie.js
@@ -0,0 +1,163 @@
+import Toast from "react-bootstrap/Toast";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./Movie.css";
+import { API_URL, getToken } from "../../util/Util";
+import Swal from "sweetalert2";
+
+export const Movie = () => {
+  const params = useParams();
+  const [movieId, setMovieId] = useState("");
+  const [movie, setMovie] = useState({});
+  const [score, setScore] = useState([]);
+  const [scoreSelected, setScoreSelected] = useState("");
+  const [scoreId, setScoreId] = useState("");
+
+  useEffect(() => {
+    //getMovies();
+    setMovieId(params.id);
+    getMovie();
+    setScoreData();
+    checkScore();
+  }, [scoreSelected]);
+
+  const getMovie = async () => {
+    let response = await fetch(API_URL + "movie/" + params.id);
+    response = await response.json();
+    setMovie(response);
+  };
+
+  const checkScore = async () => {
+    const requestData = {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: getToken(),
+      },
+    };
+    let response = await fetch(
+      API_URL + "score/check/" + params.id,
+      requestData
+    );
+    response = await response.json();
+    //const response = { id: "6399cfadc9e9a77c999e8306", score: 9};
+    if (response.id !=null && response.score != null) {
+      setScoreSelected(response.score);
+      setScoreId(response.id);
+    }
+  };
+
+  const sendScoreApi = async (score) => {
+    let method = "post";
+    if (scoreSelected != "") {
+      method = "put";
+    }
+
+      const scoreDTO = {
+      score: score,
+      movieId: movieId,
+    };
+
+    const requestData = {
+      method,
+      body: JSON.stringify(scoreDTO),
+      headers: {
+        "Content-type": "application/json",
+        Authorization: getToken(),
+      },
+    };
+    let response = await fetch(API_URL + "score/", + scoreId, requestData);
+    response = await response.json();
+    if (response.status == true) {
+      Swal.fire({
+        title: "",
+        text: response.message,
+        icon: "success",
+        confirmButtonText: "ok",
+      });
+    } else {
+      Swal.fire({
+        title: "",
+        text: response.message,
+        icon: "warning",
+        confirmButtonText: "Reintentar",
+      });
+    }
+  };
+
+  const setScoreData = () => {
+    const scores = [];
+    for (let index = 1; index <= 10; index++) {
+      scores.push(index);
+    }
+    setScore(scores);
+  };
+
+  const sendScore = async (event) => {
+    const { value } = event.target;
+    await sendScoreApi(value);
+  };
+
+  const message = () => (
+    <Toast>
+      <Toast.Header>
+        <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+        <strong className="me-auto">Bootstrap</strong>
+        <small>11 mins ago</small>
+      </Toast.Header>
+      <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+    </Toast>
+  );
+
+  return (
+    <div className="movie-container">
+      <iframe
+        id="myVideo"
+        width="560"
+        height="515"
+        src={
+          !movie.trailerLink
+            ? "https://www.youtube.com/embed/4Lp-Vc4i2QI"
+            : movie.trailerLink
+        }
+        title={movie.name}
+        frameBorder ="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      <div className="main-container">
+        <div className="content">
+          <h1>{movie.name}</h1>
+          <p>{movie.description}</p>
+          <div className="staff-list">
+            {movie.staffList && movie.staffList.length > 0
+              ? movie.staffList.map((staff, idx) => (
+                  <p key={idx}>
+                    {staff.name} {staff.lastName} ({staff.rol})
+                  </p>
+                ))
+              : "No hay elenco definido"}
+          </div>
+          <div className="category-list">
+            {movie.categories && movie.categories.length > 0
+              ? movie.categories.map((staff, idx) => (
+                  <p key={idx}>
+                    {staff.name} {staff.lastName} {staff.rol}
+                  </p>
+                ))
+              : "No hay categorías definidas"}
+          </div>
+          <div className="rate">
+            <p>Calificar pelicula</p>
+            <select value={scoreSelected} onChange={sendScore}>
+              <option> Sin calificar</option>
+              {score.map((element, idx) => (
+                <option key={idx}>{element}</option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
