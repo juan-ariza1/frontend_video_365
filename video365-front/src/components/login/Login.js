fb79f2a4f6ce736c414fc34bf39a72aef44ddce2
Andrux365 <andres.romero1@o365.unab.edu.co>
2022-12-16 15:06:08 -0500

    [Feature] Added the last part of the login.js

--- video365-front/src/components/login/Login.js
+++ video365-front/src/components/login/Login.js
@@ -28,37 +28,69 @@ export const Login = ()=> {
         const value = event.target.value;
         setFormData((values) => ({ ...values, [name]: value }));
     };
-    
+
     const handleSubmit = async (event) => {
-    event.preventDefault();
-    const response = await sendAuthApi();
+        event.preventDefault();
+        const response = await sendAuthApi();
 
-    if(response && response.token && response.token != "") {
-        localStorage.setItem("authData", JSON.stringify(response));
-        Navigate("/movies");
-    } else {
-        Swal.fire({
-            title: "Error!",
-            text: "Credenciales no validas",
-            icon: "error",
-            confirmButtonText: "Reintentar",
-        });
-    }
-};
-
-const sendAuthApi = async () => {
-    const requestData = {
-        method: "POST",
-        body: JSON.stringify(formData),
-        headers: {
+        if (response && response.token && response.token != "") {
+            localStorage.setItem("authData", JSON.stringify(response));
+            navigate("/movies");
+          } else {
+            Swal.fire({
+              title: "Error!",
+              text: "Credenciales no válidas",
+              icon: "error",
+              confirmButtonText: "Reintentar",
+            });
+        }
+      };
+    
+      const sendAuthApi = async () => {
+        const requestData = {
+          method: "POST",
+          body: JSON.stringify(formData),
+          headers: {
             "Content-type": "application/json",
-        },
-    };
-    let response = await fetch(API_URL + "auth", requestData);
-    response = await response.json();
-    return response;
-};
+          },
+        };
+        let response = await fetch(API_URL + "auth", requestData);
+        response = await response.json();
+        return response;
+      };
+    
+      return (
+        <div className="row justify-content-center">
+          <div className="col-8 col-sm-8 col-md-4 col-lg-3">
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="user"
+                  onChange={handleChange}
+                />
+              </Form.Group>
+    
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Registra tu contraseña"
+                  name="password"
+                  onChange={handleChange}
+                />
+              </Form.Group>
+    
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+            <Link to="/register">Registrarse</Link>
+          </div>
+        </div>
+      );
+      };
 
-
-
-
+    
