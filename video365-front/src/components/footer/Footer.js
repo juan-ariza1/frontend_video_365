8ea3d9018983a6f03afa75ed533df051fdc82389
osmawil <oscar.wilches.mt@usa.edu.co>
2022-12-15 23:27:49 -0500

    [feature] agregadas funciones de auth en el login creacion de componente footer

---
+++ video365-front/src/components/footer/Footer.js
@@ -0,0 +1,20 @@
+import "./Footer.css";
+
+import React from "react";
+
+
+class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="footer">
+                Todos los derechos reservados
+            </div>
+        );
+    }
+}
+
+export default Footer;
